f21e2020e6f5f01071095f53f62be001fb7349b7
keylanguiano <keyla_anguiano@hotmail.com>
2024-05-13 08:35:01 -0600

    Estructura inicial

---
+++ pages/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-col class="ma-0 pa-0 justify-center align-center" align="center" justify="center">
+    <v-row class="ma-0 pa-0 justify-center align-center" style="background-color: #100F57 !important; height: 600px !important;">
+      <v-col class="ma-0 pa-0">
+        <v-row class="ma-0 pa-0 mt-16 justify-center align-center">
+          <v-btn rounded class="ma-0 pa-6 home-button">
+            <span class="ma-0 pa-0 home-button-text">Get your free consultation now</span>
+          </v-btn>
+        </v-row>
+
+        <v-row class="ma-0 pa-0 mt-10 justify-center align-center">
+          <p class="ma-0 pa-0 home-banner-title">
+            Manage your team easily with task man
+          </p>
+        </v-row>
+
+        <v-row class="ma-0 pa-0 mt-5 justify-center align-center">
+          <p class="home-banner-text">
+            Statisdaa is a  school management solution that offers a personalized portal to each type of user
+          </p>
+        </v-row>
+
+        <v-row class="ma-0 pa-0 mt-10 justify-center align-center">
+          <v-btn rounded class="ma-0 pa-6 home-button-principal">
+            <span class="ma-0 pa-0 home-button-principal-text">get started </span>
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <v-row class="ma-0 pa-0" style="height: 800px !important;">
+      <v-col class="ma-0 pa-0">
+        <v-row justify="center" style="height: 200px; background-color: #100F57 !important;">
+          <img class="ma-0 pa-0" width="931" src="../assets/home/boxboard.png">
+        </v-row>
+
+        <v-row style="background-color: white !important; height: 100px !important;" />
+      </v-col>
+    </v-row>
+
+    <v-row class="ma-0 pa-0 align-center justify-center" align="center" justify="center">
+      <p class="ma-0 pa-0 align-center justify-center" style="font-size: 36px; font-weight: 600;" align="center" justify="center">Trusted by company like</p>
+    </v-row>
+
+    <v-row class="ma-0 pa-0 align-center justify-center mt-10" align="center" justify="center" style="width: 50%; align-content: center !important; justify-content: center !important; align-items: center;">
+      <v-col cols="1" class="ma-0 pa-0">
+        <img class="ma-0 pa-0" height="35.85" src="../assets/home/1.svg">
+      </v-col>
+
+      <v-spacer />
+
+      <v-col cols="1" class="ma-0 pa-0">
+        <img class="ma-0 pa-0" height="35.85" src="../assets/home/2.svg">
+      </v-col>
+
+      <v-spacer />
+
+      <v-col cols="1" class="ma-0 pa-0">
+        <img class="ma-0 pa-0" height="35.85" src="../assets/home/3.svg">
+      </v-col>
+
+      <v-spacer />
+
+      <v-col cols="1" class="ma-0 pa-0">
+        <img class="ma-0 pa-0" height="35.85" src="../assets/home/1.svg">
+      </v-col>
+
+      <v-spacer />
+
+      <v-col cols="1" class="ma-0 pa-0">
+        <img class="ma-0 pa-0" height="35.85" src="../assets/home/2.svg">
+      </v-col>
+
+      <v-spacer />
+
+      <v-col cols="1" class="ma-0 pa-0">
+        <img class="ma-0 pa-0" height="35.85" src="../assets/home/3.svg">
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: 'ui-home'
+}
+</script>
